df5212642ac6816f78e2fa4c99fc1a98188e811d
sergemad <47142156+sergemad@users.noreply.github.com>
2021-12-07 15:20:42 +0100

    Add files via upload

---
+++ main_app.py
@@ -0,0 +1,201 @@
+import streamlit as st
+import numpy as np
+import dlib 
+import cv2
+
+# Extracting index from array
+def extract_index_nparray(nparray):
+    index = None
+    for num in nparray[0]:
+        index = num
+        break
+    return index
+
+#_lock = RendererAgg.lock
+
+#Setting Title of App
+st.title("Face swapping")
+st.markdown("Upload two images")
+
+#Uploading the dog image
+row0_space1, row0_1, row0_space2, row0_2, row0_space3 = st.columns((.1, 1, .1, 1, .1))
+with row0_1:
+    Face1 = st.file_uploader("Choose the first face")
+with row0_2:
+    Face2= st.file_uploader("Choose the second face")
+submit = st.button('Swap')
+#On predict button click
+if submit:
+
+
+    if Face1 is not None and Face2 is not None:
+
+        # Convert the file to an opencv image.
+        file_bytes1 = np.asarray(bytearray(Face1.read()), dtype=np.uint8)
+        opencv_face1= cv2.imdecode(file_bytes1, 1)
+        #face1 = cv2.resize(opencv_face1, (500,300))
+    
+        file_bytes2 = np.asarray(bytearray(Face2.read()), dtype=np.uint8)
+        opencv_face2= cv2.imdecode(file_bytes2, 1)
+        #face2 = cv2.resize(opencv_face2, (500,300))
+
+
+        img = np.array(opencv_face1)
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        mask = np.zeros_like(img_gray)
+        img2 = np.array(opencv_face2)
+        img2_gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+        detector = dlib.get_frontal_face_detector()
+        predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+        height, width, channels = img2.shape
+        img2_new_face = np.zeros((height,width, channels), np.uint8)
+
+        # Face 1
+        faces = detector(img_gray)
+        for face in faces:
+            landmarks = predictor(img_gray, face)
+            landmarks_points = []
+            for n in range(0, 68):
+                x = landmarks.part(n).x
+                y = landmarks.part(n).y
+                landmarks_points.append((x, y))
+
+            points = np.array(landmarks_points, np.int32)
+            convexhull = cv2.convexHull(points) # Convex hull(enveloppe convexe) of the point set
+            cv2.fillConvexPoly(mask, convexhull, 255) # draw any convex polygone
+
+            face_image_1 = cv2.bitwise_and(img, img, mask=mask)
+            # Delaunay triangulation
+            rect = cv2.boundingRect(convexhull) # catch the point of the parameter 
+            subdiv = cv2.Subdiv2D(rect)
+            subdiv.insert(landmarks_points)
+            triangles = subdiv.getTriangleList()
+            triangles = np.array(triangles, dtype=np.int32)
+
+            indexes_triangles = []
+            for t in triangles:
+                pt1 = (t[0], t[1])
+                pt2 = (t[2], t[3])
+                pt3 = (t[4], t[5])
+
+
+                index_pt1 = np.where((points == pt1).all(axis=1))
+                index_pt1 = extract_index_nparray(index_pt1)
+
+                index_pt2 = np.where((points == pt2).all(axis=1))
+                index_pt2 = extract_index_nparray(index_pt2)
+
+                index_pt3 = np.where((points == pt3).all(axis=1))
+                index_pt3 = extract_index_nparray(index_pt3)
+
+                if index_pt1 is not None and index_pt2 is not None and index_pt3 is not None:
+                    triangle = [index_pt1, index_pt2, index_pt3]
+                    indexes_triangles.append(triangle)
+            
+        #st.image(face_image_1,channels="BGR")
+
+        # Face 2
+        faces2 = detector(img2_gray)
+        for face in faces2:
+            landmarks = predictor(img2_gray, face)
+            landmarks_points2 = []
+            for n in range(0, 68):
+                x = landmarks.part(n).x
+                y = landmarks.part(n).y
+                landmarks_points2.append((x, y))
+
+
+            points2 = np.array(landmarks_points2, np.int32)
+            convexhull2 = cv2.convexHull(points2)
+
+        # Creating empty mask
+        lines_space_mask = np.zeros_like(img_gray)
+        lines_space_new_face = np.zeros_like(img2)
+
+        # Triangulation of both faces
+        for triangle_index in indexes_triangles:
+            # Triangulation of the first face
+            tr1_pt1 = landmarks_points[triangle_index[0]]
+            tr1_pt2 = landmarks_points[triangle_index[1]]
+            tr1_pt3 = landmarks_points[triangle_index[2]]
+            triangle1 = np.array([tr1_pt1, tr1_pt2, tr1_pt3], np.int32)
+
+
+            rect1 = cv2.boundingRect(triangle1)
+            (x, y, w, h) = rect1
+            cropped_triangle = img[y: y + h, x: x + w]
+            cropped_tr1_mask = np.zeros((h, w), np.uint8)
+
+
+            points = np.array([[tr1_pt1[0] - x, tr1_pt1[1] - y],
+                            [tr1_pt2[0] - x, tr1_pt2[1] - y],
+                            [tr1_pt3[0] - x, tr1_pt3[1] - y]], np.int32)
+
+            cv2.fillConvexPoly(cropped_tr1_mask, points, 255)
+
+            # Lines space
+            cv2.line(lines_space_mask, tr1_pt1, tr1_pt2, 255)
+            cv2.line(lines_space_mask, tr1_pt2, tr1_pt3, 255)
+            cv2.line(lines_space_mask, tr1_pt1, tr1_pt3, 255)
+            lines_space = cv2.bitwise_and(img, img, mask=lines_space_mask)
+
+            # Triangulation of second face
+            tr2_pt1 = landmarks_points2[triangle_index[0]]
+            tr2_pt2 = landmarks_points2[triangle_index[1]]
+            tr2_pt3 = landmarks_points2[triangle_index[2]]
+            triangle2 = np.array([tr2_pt1, tr2_pt2, tr2_pt3], np.int32)
+
+
+            rect2 = cv2.boundingRect(triangle2)
+            (x, y, w, h) = rect2
+
+            cropped_tr2_mask = np.zeros((h, w), np.uint8)
+
+            points2 = np.array([[tr2_pt1[0] - x, tr2_pt1[1] - y],
+                                [tr2_pt2[0] - x, tr2_pt2[1] - y],
+                                [tr2_pt3[0] - x, tr2_pt3[1] - y]], np.int32)
+
+            cv2.fillConvexPoly(cropped_tr2_mask, points2, 255)
+
+            # Warp triangles
+            points = np.float32(points)
+            points2 = np.float32(points2)
+            M = cv2.getAffineTransform(points, points2)
+            warped_triangle = cv2.warpAffine(cropped_triangle, M, (w, h))
+            warped_triangle = cv2.bitwise_and(warped_triangle, warped_triangle, mask=cropped_tr2_mask)
+
+            # Reconstructing destination face
+            img2_new_face_rect_area = img2_new_face[y: y + h, x: x + w]
+            img2_new_face_rect_area_gray = cv2.cvtColor(img2_new_face_rect_area, cv2.COLOR_BGR2GRAY)
+            _, mask_triangles_designed = cv2.threshold(img2_new_face_rect_area_gray, 1, 255, cv2.THRESH_BINARY_INV)
+            warped_triangle = cv2.bitwise_and(warped_triangle, warped_triangle, mask=mask_triangles_designed)
+
+            img2_new_face_rect_area = cv2.add(img2_new_face_rect_area, warped_triangle)
+            img2_new_face[y: y + h, x: x + w] = img2_new_face_rect_area
+
+        # Face swapped (putting 1st face into 2nd face)
+        img2_face_mask = np.zeros_like(img2_gray)
+        img2_head_mask = cv2.fillConvexPoly(img2_face_mask, convexhull2, 255)
+        img2_face_mask = cv2.bitwise_not(img2_head_mask)
+
+        img2_head_noface = cv2.bitwise_and(img2, img2, mask=img2_face_mask)
+        result = cv2.add(img2_head_noface, img2_new_face)
+        #st.image(result, channels="BGR")
+
+        # Creating seamless clone of two faces
+        (x, y, w, h) = cv2.boundingRect(convexhull2)
+        center_face2 = (int((x + x + w) / 2), int((y + y + h) / 2))
+        seamlessclone = cv2.seamlessClone(result, img2, img2_head_mask, center_face2, cv2.NORMAL_CLONE)
+
+        row1_space1, row1_1, row1_space2, row1_2, row1_space3, row1_3, row1_space4 = st.columns((.1, 1, .1, 1, .1, 1, .1))
+        with row1_1:
+            st.header('First image')
+            st.image(opencv_face1, channels="BGR")
+        with row1_2:
+            st.header('Second image')
+            st.image(opencv_face2, channels="BGR")
+        with row1_3:
+            st.header('New face')
+            st.image(seamlessclone,channels="BGR")
+        
